9a1ce4d8bf3b3f8ed45d355b282a2955a6e4fbbc
wmosienski <w.mosienski@gmail.com>
2022-11-17 14:30:59 +0100

    rem unused import

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import { AuthMiddleware } from "@Controllers/middlewares/auth.middleware";
 import { UserController } from "@Controllers/user.controller";
 import initializeMongo from "@Database/mongo/mongo";
 import { LoggerService, UserService } from "@Services";
